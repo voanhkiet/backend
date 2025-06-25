2d885045d4ec7471e005145296976945729503eb
voanhkiet <voanhkiet261192@gmail.com>
2025-06-25 22:23:12 +0700

    backend

--- server.js
+++ server.js
@@ -33,8 +33,7 @@ app.use(cors({
   credentials: true
 }));
 
-// This line handles preflight OPTIONS requests for all routes
-app.options("*", cors());
+
 
 app.use(require("./middleware/errorHandler"));
 
