fdc7c0f79866edad513c80b15d127da0b25d7423
voanhkiet <voanhkiet261192@gmail.com>
2025-06-25 11:32:27 +0700

    update cloud

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
-const token = req.header("Authorization")?.split(" ")[1];
+
 const authMiddleware = function (req, res, next) {
     const token = req.header("Authorization")?.split(" ")[1];
     if (!token) return res.status(401).json({ message: "Access denied" });
 
     try {
         const verified = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verified;
+        req.user = decoded;
         next();
     } catch (err) {
-        res.status(400).json({ message: "Invalid token" });
+        res.status(401).json({ message: "Invalid token" });
     }
 };
 
